4f48279432764816feeddfc47d4a6863e8bbdda5
whitelee <shiqiba7@163.com>
2014-02-22 15:11:48 +0800

    admin autoresponse operations

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -19,9 +19,32 @@ class Admin extends CI_Controller {
 	 */
 	public function index()
 	{
-		echo "this is admin";
+		$this->load->view('admin');
+	}
+	public function wechat($item='',$op=''){
+		switch($item){
+			case '':
+				echo 'add pages later ,wechat background index page';
+				break;//index page
+			case 'autoresponse':
+				if($op=='add'){//add auto response items
+					if(!file_exists('application/views/admin/wechat/autoreponse/add.php')){
+						show_404();
+					}
+					
+					$this->load->view('admin/wechat/autoreponse/add.php');
+					
+				}elseif($op=='del'){
+					echo 'del autoresonse item ,unfinished';
+				}else{
+					echo 'other operations';
+				}
+				break;//case autoresponse
+			default:
+				show_404();
+		}
 	}
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+/* End of file admin.php */
+/* Location: ./application/controllers/admin.php */
