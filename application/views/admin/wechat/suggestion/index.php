877b8bc6d0028d6d99dfa36cd730c250fc0baf5e
whitelee <shiqiba7@163.com>
2014-02-27 22:26:02 +0800

    make the page of suggestion more beautiful

--- application/views/admin/wechat/suggestion/index.php
+++ application/views/admin/wechat/suggestion/index.php
@@ -1,12 +1,12 @@
 <a href=<?php echo $base_url.'index.php/admin/wechat/suggestion/add';?>>添加</a>
 <br>
-<div class="Id">ID</div><div class="title">TITLE</div><div class="answer">ANSWER</div><br>
+<div class="Id">ID</div><div class="username">USERNAME</div><div class="answer">SUGGESTION</div><div class="flag">FLAG</div><br>
 <?php foreach($suggestion_list as $item):?>
 <div class="items">
 <div class="Id"><?php echo $item['id'];?></div>
 <div class="username"><?php echo $item['username'];?></div>
 <div class="suggestion"><?php echo $item['suggestion'];?></div>
-
+<div class="flag"><?php echo $item['flag'];?></div>
 <div class="active"><a href=<?php if($item['isactive']==1){$act='Setinactive';}else{$act='Setactive';}echo $base_url.'index.php/admin/wechat/suggestion/'.$act.'/'.$item['id'];?>><?php echo $act;?></a></div>
 </div>
 <?php endforeach;?>
